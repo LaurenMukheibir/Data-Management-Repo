d2db40b512dafa91af6d98ccc7f0a4b0aa0af8c8
Lauren Mukheibir <MKHLAU004@myuct.ac.za>
2025-02-26 18:05:20 +0200

    Conducting Analyses

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -4,10 +4,12 @@
 
 #Installing Packages 
 install.packages("readr")
+install.packages("report")
 
 #Loading Libraries
 library(readr)
 library(tidyverse)
+library(report)
 
 #Loading in Data 
 data <- read.csv("raw_data_species_richness_total_abundance.csv")
@@ -27,6 +29,7 @@ view(data_clean)
 #Renaming Columns 
 colnames(data_new) <- c("Macrophyte_Type", "Amphipod", "Isopod", "Shrimp","Caprellid", "Copepod", "Tanaid", "Polychaete_Motile", "Hermit_Crab", "Sea_Spider", "Anemone", "Sea_Cucumber", 'Brittle_Star', "Feather_Hydroid", "Urchin", "Gastropod", "Fanworm", "Polychaetes_Sessile", "Ascidian_Colonial", "Fish","SR", "TA", "SW_Index")
 
+#Renaming Macrophyte Type to full names
 data_new <- data_new %>%
   mutate(Macrophyte_Type = case_when (
     str_detect(Macrophyte_Type, "Sarg") ~ "Sargassum",
@@ -39,4 +42,31 @@ data_new <- data_new %>%
 
 ggplot(data_new, aes(x=Macrophyte_Type, y=SR)) +
   geom_bar(stat="identity") +
-  labs(title="Species Richness by Macrophyte Type", x= "Macrophyte Type", y= "Species Richness")
+  labs(title="Species Richness by Macrophyte Type", x= "Macrophyte Type", y= "Species Richness") +
+  theme_minimal()
+  
+
+### Boxplot without outliers
+ggplot(data_new, aes(x=Macrophyte_Type, y=TA, fill=Macrophyte_Type)) +
+  geom_boxplot(outlier.shape = NA) +
+  labs(x= "Macrophyte Type", y= "Total Abundance") +
+  ylim(0,60) +
+  scale_fill_manual(values = c("Amphibolis" = "lightpink", "Ecklonia" = "lightblue", "Sargassum"="lightgreen"))
+
+
+#Using Amphipod data
+ggplot(data_new, aes(x=Macrophyte_Type, y=Amphipod, fill=Macrophyte_Type)) +
+  geom_bar(stat="identity") +
+  labs( title= "Amphipod Count against Macrophyte Type", x= "Macrophyte Type", y= "Amphipod Count") +
+  scale_fill_manual (values= c("Amphibolis" ="lightpink", "Ecklonia" = "lightblue", "Sargassum"="lightgreen"))
+  
+#Statistical Analysis
+aov_SR <- aov(SR ~ Macrophyte_Type, data = data_new)
+aov_SR #results of anova test
+
+aov_TA <- aov(TA ~ Macrophyte_Type, data = data_new)
+aov_TA
+
+report(aov_SR)
+report(aov_TA)
+
